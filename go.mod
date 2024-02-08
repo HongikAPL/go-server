0e428a6d3ee4ecdd9d940df2b9bb4a9d21caf484
younggyo <lee4519698@hanmail.net>
2024-02-08 20:17:25 +0900

    feat : create accessToken

--- go.mod
+++ go.mod
@@ -12,10 +12,13 @@ require (
 	github.com/cli/safeexec v1.0.1 // indirect
 	github.com/cosmtrek/air v1.49.0 // indirect
 	github.com/creack/pty v1.1.21 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gohugoio/hugo v0.122.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -26,9 +29,11 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type ResponseDto struct {
-	Result string `json:"result"`
+type SigninResponseDto struct {
+	AccessToken string `json:"accessToken"`
 }
 
-type Response struct {
-	Status int `json:"status"`
-	Data interface{} `json:"data"`
-	Message string `json:"message"`
+type ApiResponse struct {
+	Status  int         `json:"status"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
 }
 
 type User struct {
@@ -23,36 +28,79 @@ type User struct {
 	Password string `json:"password"`
 }
 
+type AuthTokenClaims struct {
+	TokenUUID string   `json:"tid"`
+	Username  string   `json:"username"`
+	Name      string   `json:"name"`
+	Role      []string `json:"role"`
+	jwt.StandardClaims
+}
+
 const (
 	validUsername = "test"
 	validPassword = "test"
 )
 
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func generateToken(username string) (string, error) {
+	secretKey := os.Getenv("SECRET_KEY")
+	expirationTime := time.Now().Add(1 * time.Hour)
+
+	claims := AuthTokenClaims{
+		TokenUUID: uuid.NewString(),
+		Username:  username,
+		Name:      "younggyo",
+		Role:      []string{"user"},
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(secretKey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
 func loginHandler(c echo.Context) error {
 	var user User
+
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, "Invalid request payload")
 	}
 
 	if user.Username == validUsername && user.Password == validPassword {
-		responseDto := ResponseDto{Result : "성공"}
-		return c.JSON(http.StatusOK, Response{Status: http.StatusOK, Data: responseDto, Message: "Authentication Success"})
+		accessToken, err := generateToken(user.Username)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, "Failed to generate JWT token")
+		}
+
+		SigninResponseDto := SigninResponseDto{AccessToken: accessToken}
+		return c.JSON(http.StatusOK, ApiResponse{Status: http.StatusOK, Data: SigninResponseDto, Message: "Authentication Success"})
 	} else {
 		return echo.NewHTTPError(http.StatusBadRequest, "Authentication failed. Invalid username or password")
 	}
 }
 
 func main() {
+	loadEnv()
+
 	e := echo.New()
 
-	// 미들웨어 설정
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// 라우트 설정
-	e.POST("/api/auth/login", loginHandler)
+	e.POST("/api/auth/signin", loginHandler)
 
-	// 서버 시작
-	fmt.Println("Server is running on http://localhost:8080")
-	e.Start(":8080")
+	e.Logger.Fatal(e.Start(":8080"))
 }
